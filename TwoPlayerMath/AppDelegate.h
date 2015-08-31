941917b07f6baf4bfd73444e3829c55e1ee670b5
katiepeterso <katiepeterso@outlook.com>
2015-08-31 14:12:17 -0700

    initial commit

---
+++ TwoPlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoPlayerMath
+//
+//  Created by Katherine Peterson on 2015-08-31.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
